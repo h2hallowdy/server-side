89b72a8416608b28c03144ae921f6924115f3036
Hoang <info@coders.tokyo>
2018-09-30 10:29:08 +0700

    lesson 15-cookie done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var cookieParser = require('cookie-parser');
 
 
 
@@ -13,7 +13,7 @@ app.set('view engine', 'pug');
 app.set('views', './views')
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+app.use(cookieParser());
 // var users = [
 // 	{ id: 1, name: 'Hoang'}, 
 // 	{ id: 2, name: 'Huy'},
